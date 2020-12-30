eef6798ded7a5d3731256c7b4110d7145974b7bd
seangao14 <y_gao@umail.ucsb.edu>
2020-12-30 15:05:11 -0800

    im going to fucking cry

--- popup.js
+++ popup.js
@@ -18,17 +18,32 @@ document.addEventListener('DOMContentLoaded', function(){
         
     }
 
-    function apply(){
+    async function apply(){
         /*items.forEach(item => {
 
             const div = document.createElement('div')
             div.textContent = `${item}`
             document.body.appendChild(div)
         })*/
+        
+        const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+        const pdfbytes = await fetch(url).then(res => res.arrayBuffer())
 
-        var filename = document.getElementById("filename").value
+        //const pdfbytes = new Response("USPSlabel.pdf").arrayBuffer()
 
-        alert(browser.downloads.showDefaultFolder())
+        const pdfDoc = PDFDocument.load(pdfbytes)
+        const pages = pdfDoc.getPages()
+        const firstPage = pages[0]
+        const {width, height} = firstPage.getSize()
+
+        for (i = 0; i < pages.length; i++) {
+            pages[i].drawText(`GEORGE IS A GAY MAN ${i}`, {
+                x: 100,
+                y: height/2 - 300,
+                size: 20,
+            })
+        }
+        
     }
 
 },false)
