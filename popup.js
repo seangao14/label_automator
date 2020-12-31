96a62b60c0637d99429c1d52c103989ae324e130
seangao14 <y_gao@umail.ucsb.edu>
2020-12-30 23:07:17 -0800

    im not longer crying

--- popup.js
+++ popup.js
@@ -1,12 +1,13 @@
 var items
 const {PDFDocument} = PDFLib
 
+
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#fetch').addEventListener('click', fetch, false)
+    document.querySelector('#fetch').addEventListener('click', retrieve, false)
     document.querySelector('#apply').addEventListener('click', apply, false)
 
 
-    function fetch(){
+    function retrieve(){
         chrome.tabs.query({currentWindow: true, active: true},
             function(tabs){
                 chrome.tabs.sendMessage(tabs[0].id, 'hi', setItems)
@@ -26,24 +27,41 @@ document.addEventListener('DOMContentLoaded', function(){
             document.body.appendChild(div)
         })*/
         
-        const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+        //const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+        const url = "USPSlabel.pdf"
         const pdfbytes = await fetch(url).then(res => res.arrayBuffer())
 
         //const pdfbytes = new Response("USPSlabel.pdf").arrayBuffer()
 
-        const pdfDoc = PDFDocument.load(pdfbytes)
+        const pdfDoc = await PDFDocument.load(pdfbytes)
         const pages = pdfDoc.getPages()
         const firstPage = pages[0]
         const {width, height} = firstPage.getSize()
 
-        for (i = 0; i < pages.length; i++) {
+        for (let i = 0; i < pages.length; i++) {
             pages[i].drawText(`GEORGE IS A GAY MAN ${i}`, {
                 x: 100,
                 y: height/2 - 300,
                 size: 20,
             })
         }
+
+        const pdfBytes = await pdfDoc.save()
+
+        /*
+        var sample = base64ToArrayBuffer(pdfBytes)
+        var url_ = URL.createObjectURL(sample)
+        */
+
+        var bytes = new Uint8Array(pdfBytes); // pass your byte response to this constructor 
+        var blob=new Blob([bytes], {type: "application/pdf"});// change resultByte to bytes
+        var url_ = URL.createObjectURL(blob)
+    
         
+        chrome.downloads.download({
+            url: url_, 
+            filename: "fun_with_js.pdf"
+        })
     }
 
 },false)
