2f7e1305e492ad2058ce1513e19c2b4287031591
seangao14 <y_gao@umail.ucsb.edu>
2020-12-31 14:28:24 -0800

    changed to upload

--- popup.js
+++ popup.js
@@ -3,7 +3,9 @@ const {PDFDocument} = PDFLib
 
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#fetch').addEventListener('click', retrieve, false)
-    document.querySelector('#apply').addEventListener('click', apply, false)
+    document.querySelector('#fileUpload').addEventListener('change', event => {
+        handleUpload(event)
+    })
 
 
     function retrieve(){
@@ -11,6 +13,56 @@ document.addEventListener('DOMContentLoaded', function(){
             function(tabs){
                 chrome.tabs.sendMessage(tabs[0].id, 'hi')
             })
+    }
+
+    async function handleUpload(event)  {
+        const files = event.target.files
+
+        console.log(files[0])
+
+        //const pdfBytes = files[0].arrayBuffer()
+        //console.log(pdfBytes)
+
+        let fr = new FileReader()
+        const pdfBytes = fr.readAsArrayBuffer(files[0])
+
+        const pdfDoc = await PDFDocument.load(pdfBytes)
+        console.log('AFTER AWAIT')
+
+        
+        
+        const pages = pdfDoc.getPages()
+        const firstPage = pages[0]
+        const {width, height} = firstPage.getSize()
+
+        const bg = chrome.extension.getBackgroundPage()
+
+        for (let i = 0; i < bg.items.length; i++) {
+            pages[i].drawText(`${bg.items[i]}`, {
+                x: 20,
+                y: height/2 - 300,
+                size: 8,
+            })
+
+            pages[i].drawText(`${bg.qty[i]}`, {
+                x: 20,
+                y: height/2 - 320,
+                size: 8,
+            })
+        }
+
+        const newBytes = await pdfDoc.save()
+        console.log(newBytes)
+
+        var bytes = new Uint8Array(newBytes); // pass your byte response to this constructor 
+        var blob=new Blob([bytes], {type: "application/pdf"});// change resultByte to bytes
+        var url_ = URL.createObjectURL(blob)
+    
+        
+        chrome.downloads.download({
+            url: url_, 
+            filename: "new_label.pdf"
+        })
     }
 
     async function apply(){
