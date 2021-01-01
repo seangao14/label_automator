f94c175b4f2037cfba8c7018e9f722a5308f3f4f
seangao14 <y_gao@umail.ucsb.edu>
2020-12-31 22:18:33 -0800

    wow it did the thing

--- popup.js
+++ popup.js
@@ -24,8 +24,53 @@ document.addEventListener('DOMContentLoaded', function(){
         //console.log(pdfBytes)
 
         let fr = new FileReader()
-        const pdfBytes = fr.readAsArrayBuffer(files[0])
 
+        fr.onload = async function(e){
+            let file_ = fr.result
+ 
+            console.log(file_)
+            const pdfDoc = await PDFDocument.load(file_)
+            console.log('AFTER AWAIT')
+
+            
+            const pages = pdfDoc.getPages()
+            const firstPage = pages[0]
+            const {width, height} = firstPage.getSize()
+
+            const bg = chrome.extension.getBackgroundPage()
+
+            for (let i = 0; i < bg.items.length; i++) {
+                pages[i].drawText(`${bg.items[i]}`, {
+                    x: 20,
+                    y: height/2 - 300,
+                    size: 8,
+                })
+
+                pages[i].drawText(`${bg.qty[i]}`, {
+                    x: 20,
+                    y: height/2 - 320,
+                    size: 8,
+                })
+            }
+
+            const newBytes = await pdfDoc.save()
+            console.log(newBytes)
+
+            var bytes = new Uint8Array(newBytes); // pass your byte response to this constructor 
+            var blob=new Blob([bytes], {type: "application/pdf"});// change resultByte to bytes
+            var url_ = URL.createObjectURL(blob)
+        
+            
+            chrome.downloads.download({
+                url: url_, 
+                filename: "new_label.pdf"
+            })
+
+        }
+
+        fr.readAsArrayBuffer(files[0]) 
+
+        /*
         const pdfDoc = await PDFDocument.load(pdfBytes)
         console.log('AFTER AWAIT')
 
@@ -63,6 +108,7 @@ document.addEventListener('DOMContentLoaded', function(){
             url: url_, 
             filename: "new_label.pdf"
         })
+        */
     }
 
     async function apply(){
